db81aa62d390348f47c78b92e612ab4a00415ff8
Binh Nguyen <bnguyen@manetu.com>
2020-12-21 17:18:23 -0500

    Use loop on testapp until exit to allow the user continuous testing without having to start the app again and again.
    Signed-off-by: Binh Nguyen <bnguyen@manetu.com>

--- testapp/main.go
+++ testapp/main.go
@@ -9,20 +9,18 @@ import (
 )
 
 func main() {
+	for {
+		fmt.Println("Enter password or Ctrl-c to exit:")
+		reader := bufio.NewReader(os.Stdin)
+		password, _ := reader.ReadString('\n')
+		//password := "Testaaatyhg890l33t"
 
-	fmt.Println("Enter password:")
-	reader := bufio.NewReader(os.Stdin)
-	password, _ := reader.ReadString('\n')
-	//password := "Testaaatyhg890l33t"
+		passwordStenght := zxcvbn.PasswordStrength(password, nil)
 
-	passwordStenght := zxcvbn.PasswordStrength(password, nil)
-
-	fmt.Printf(
-		`Password score    (0-4): %d
-Estimated entropy (bit): %f
-Estimated time to crack: %s%s`,
-		passwordStenght.Score,
-		passwordStenght.Entropy,
-		passwordStenght.CrackTimeDisplay, "\n",
-	)
+		fmt.Printf("Password score    (0-4): %d\nEstimated entropy (bit): %f\nEstimated time to crack: %s\n\n",
+			passwordStenght.Score,
+			passwordStenght.Entropy,
+			passwordStenght.CrackTimeDisplay,
+		)
+	}
 }
